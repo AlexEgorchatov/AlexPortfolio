3bb1921599b79bc9ce72192705c0aaa81175db0e
alex.exorchatov@gmail.com <alex.exorchatov@gmail.com>
2020-06-19 01:10:50 -0700

    Added static Content Management System for the Home page

--- AlexPortfolio/Scripts/portfolio-home.js
+++ AlexPortfolio/Scripts/portfolio-home.js
@@ -1,1 +1,25 @@
-﻿
+﻿$('#home-edit').on('click', () => {
+    $('#home-edit').css('display', 'none');
+    $('.home-apply-changes').css('display', 'inline-block');
+
+    $('#home-greeting-input').val($('#home-greeting').text());
+    $('#home-intro-input').val($('#home-intro').text());
+});
+
+$('#home-edit-save').on('click', () => {
+    $('#home-edit').css('display', 'inline-block');
+    $('.home-apply-changes').css('display', 'none');
+
+    $('#home-greeting').text($('#home-greeting-input').val());
+    $('#home-intro').text($('#home-intro-input').val());
+    $('#home-greeting-input').val('');
+    $('#home-intro-input').val('');
+});
+
+$('#home-edit-cancel').on('click', () => {
+    $('#home-edit').css('display', 'inline-block');
+    $('.home-apply-changes').css('display', 'none');
+
+    $('#home-greeting-input').val('');
+    $('#home-intro-input').val('');
+});
