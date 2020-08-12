3fad317cacd63d21ad3ea5f3669e5e6ad778531b
alex.exorchatov@gmail.com <alex.exorchatov@gmail.com>
2020-08-11 18:14:56 -0700

    Fixed the bug with Login/Register functions

--- AlexPortfolio/Models/IdentityModels.cs
+++ AlexPortfolio/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace AlexPortfolio.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("AlexPortfolioConnectionString", throwIfV1Schema: false)
         {
         }
 
