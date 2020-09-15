0e420a348a74f0b5ec604dc8cd448a3ef3aa4d5a
alex.exorchatov@gmail.com <alex.exorchatov@gmail.com>
2020-09-15 13:55:13 -0700

    Finished all pages

--- AlexPortfolio/Controllers/HobbiesController.cs
+++ AlexPortfolio/Controllers/HobbiesController.cs
@@ -17,35 +17,27 @@ namespace AlexPortfolio.Controllers
             var hobby0 = new HobbyViewModel
             {
                 HobbyId = 0,
-                Picture = "Education/VIU/viu-logo.png",
-                Titile = "Byking"
+                Picture = "Hobby/Acrobatics/Acro-logo.jpg",
+                Titile = "Acrobatics"
             };
 
             var hobby1 = new HobbyViewModel
             {
                 HobbyId = 1,
-                Picture = "Education/VIU/viu-logo.png",
-                Titile = "Hiking"
+                Picture = "Hobby/Outdoor/Outdoor-logo.jpeg",
+                Titile = "Outdoor Activities"
             };
 
             var hobby2 = new HobbyViewModel
             {
                 HobbyId = 2,
-                Picture = "Education/VIU/viu-logo.png",
-                Titile = "Acrobatics"
-            };
-
-            var hobby3 = new HobbyViewModel
-            {
-                HobbyId = 3,
-                Picture = "Education/VIU/viu-logo.png",
-                Titile = "German"
+                Picture = "Hobby/Snowboarding/Snowboard-logo.jpg",
+                Titile = "Snowboarding"
             };
 
             hobbies.Hobbies.Add(hobby0);
             hobbies.Hobbies.Add(hobby1);
             hobbies.Hobbies.Add(hobby2);
-            hobbies.Hobbies.Add(hobby3);
 
             return View(hobbies);
         }
@@ -58,61 +50,39 @@ namespace AlexPortfolio.Controllers
             switch (hobbyId)
             {
                 case 0:
-                    hobbyDetails.Title = "Byking";
-                    hobbyDetails.Description = "I do liek byking, yay!";
+                    hobbyDetails.Title = "Acrobatics";
+                    hobbyDetails.Description = "Besides programming, acrobatics is my biggest hobby. I started doing it when I was 7, and I did it for 15 years. Acrobatics is a team sport and since I moved to Canada, I was not able to engage it anymore. However, it does not mean that I forgot all of my skills. Even despite the fact that it has been four years since I officially stopped acrobatics, I am still able to perform one arm handstand, planche, press handstand, and other elements. I have a Master of Sports degree and I was twice a bronze winner of the Russian Championship";
                     hobbyDetails.Pictures = new List<string>()
                     {
-                        "Work/Inuktun/main-screen.png",
-                        "Work/Inuktun/bom-screen.png",
-                        "Work/Inuktun/bom-edit.png",
-                        "Work/Inuktun/bom-edit-comment.png",
-                        "Work/Inuktun/bom-with-comment.png",
-                        "Work/Inuktun/open-file.png",
+                        "Hobby/Acrobatics/Acro1.jpg",
+                        "Hobby/Acrobatics/Acro2.jpg",
+                        "Hobby/Acrobatics/Acro3.jpg",
+                        "Hobby/Acrobatics/Acro4.jpg",
                     };
 
                     return View(hobbyDetails);
 
                 case 1:
-                    hobbyDetails.Title = "Hiking";
-                    hobbyDetails.Description = "I do liek hiking, yay!";
+                    hobbyDetails.Title = "Outdoor Activities";
+                    hobbyDetails.Description = "I enjoy any type of outdoor activities, from climbing mountains to biking. Hiking helps me to relax after spending many hours in front of a computer screen. In addition, it is very important for health and good health makes a very positive impact on brain activity.";
                     hobbyDetails.Pictures = new List<string>()
                     {
-                        "Work/Inuktun/main-screen.png",
-                        "Work/Inuktun/bom-screen.png",
-                        "Work/Inuktun/bom-edit.png",
-                        "Work/Inuktun/bom-edit-comment.png",
-                        "Work/Inuktun/bom-with-comment.png",
-                        "Work/Inuktun/open-file.png",
+                        "Hobby/Outdoor/Outdoor1.jpg",
+                        "Hobby/Outdoor/Outdoor2.jpg",
+                        "Hobby/Outdoor/Outdoor3.jpg",
+                        "Hobby/Outdoor/Outdoor4.jpg",
                     };
 
                     return View(hobbyDetails);
 
                 case 2:
-                    hobbyDetails.Title = "Acrobatics";
-                    hobbyDetails.Description = "I do liek acrobatics, yay!";
+                    hobbyDetails.Title = "Snowboarding";
+                    hobbyDetails.Description = "\"Just try new things. Don't be afraid\". Snowboarding is my most recent hobby. I tried snowboarding for the first time in December 2019 and immediately fell in love with that. If somebody wants to take a break from work and to think of nothing but speed, one should learn how to snowboard.";
                     hobbyDetails.Pictures = new List<string>()
                     {
-                        "Work/Inuktun/main-screen.png",
-                        "Work/Inuktun/bom-screen.png",
-                        "Work/Inuktun/bom-edit.png",
-                        "Work/Inuktun/bom-edit-comment.png",
-                        "Work/Inuktun/bom-with-comment.png",
-                        "Work/Inuktun/open-file.png",
-                    };
-
-                    return View(hobbyDetails);
-
-                case 3:
-                    hobbyDetails.Title = "German";
-                    hobbyDetails.Description = "I do liek German, yay!";
-                    hobbyDetails.Pictures = new List<string>()
-                    {
-                        "Work/Inuktun/main-screen.png",
-                        "Work/Inuktun/bom-screen.png",
-                        "Work/Inuktun/bom-edit.png",
-                        "Work/Inuktun/bom-edit-comment.png",
-                        "Work/Inuktun/bom-with-comment.png",
-                        "Work/Inuktun/open-file.png",
+                        "Hobby/Snowboarding/Snowboard1.jpg",
+                        "Hobby/Snowboarding/Snowboard2.jpg",
+                        "Hobby/Snowboarding/Snowboard3.jpg",
                     };
 
                     return View(hobbyDetails);
